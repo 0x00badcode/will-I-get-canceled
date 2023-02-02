d80794197bdeb0e4255c1cbe7efa353163858063
0x00badcode <paul.crinquand@epitech.eu>
2023-02-02 22:07:00 +0100

    updated the prompt with error handling

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,7 +6,7 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = "";
+const basePromptPrefix = "You are going to act like a genZ kid that comes from woke culture. Talk in the tone of a GenZ kid posting on twitter, you don’t have the attention span to write a very structured paragraph and make everything super exagerated and overly emphasized. for each user input you are going to specify why it should be canceled. don’t do a bullet list and keep it under 280 chars, use a lot of emojis. if the prompt is not valid or asks about what the input is, just reply 'I plead the fifth' with emojis. the user input is the following :";
 const generateAction = async (req, res) => {
   // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
